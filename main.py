ee4fe882e8afa8f6c842f7d3c910ded7120bc32c
kumarsaikat321 <kumarsaikat321@gmail.com>
2021-01-07 10:15:44 +0530

    This is the code, for the least square. Once check and kindly help on this.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from Least_square import Least
+from utils.Least_square import least_sq
 
 if __name__ == '__main__':
     #print("hello world !!!!")
     #dg = DataGenerator(3, 3)
     #print(dg.generate())
-    lx = Least(5, 5)
-    print(lx.Least)
+
+    ft = least_sq(3, 3, 5)
+    print(ft.fit())
 
