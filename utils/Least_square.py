4164a2165dbc3d7c70831d0b7e19420980e4a0d7
kumarsaikat321 <kumarsaikat321@gmail.com>
2021-01-20 00:24:04 +0530

    This is code till all function. I have to update the bias and weight.check once is this ok?

--- utils/Least_square.py
+++ utils/Least_square.py
@@ -1,28 +1,54 @@
 import numpy as np
-
+import random
 
 class least_sq:
-    def __init__(self, x, y, n, w=0.0, b=0.0, weight_mat=0):
+    def __init__(self, x, y, m, b,l_s,ite=100,w_new, b_new,lr=0.001):
 
-        self.weight_mat = weight_mat
-        self.x = x  # taking input of x
+        self.l_s = l_s
+        self.x = x
         self.y = y
-        self.n = n
-        self.w = w
+        self.m = m
         self.b = b
+        self.ite = ite
+        self.w_new = w_new
+        self.b_new = b_new
+        self.lr = lr
+
+    def least_square(self):
+        self.l_s = 0
+        for i in range(len(self.x)):
+            self.l_s += np.square((self.y[i] - self.m * self.x[i] - self.b))
+        return self.l_s / len(self.x)
+
+    def update_w(self):
+        for i in range(len(self.x)):
+            self.w_new += (-1 * (2 * self.x[i] * (self.y[i] - self.m * self.x[i] - self.b)))
+        return self.w_new / len(self.x)
+
+
+    def update_b(self):
+        for i in range(len(self.x)):
+            self.b_new += (-1*(2 * (self.y[i] - self.m * self.x[i] - self.b)))
+        return self.b_new / len(self.x)
+
+
 
     def fit(self):
+        for j in range(self.ite):
+            self.x = random.random()
+            self.y = random.random()
 
-        global output
-        b = 0.001
-        for i in range(1, self.n):
-            self.w = np.array(i)
-            self.x = np.array(i)
-            self.weight_mat = self.x * self.w
-            self.weight_mat = sum(self.weight_mat)
 
-            for j in range(1, self.n):
-                self.y[j] = b + self.weight_mat
-                output = self.y[j]
 
-            return output
+
+
+
+
+
+
+
+
+
+
+
+
